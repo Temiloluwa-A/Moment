7c3d23c92eef60ef9f05da004189cc78eda655ed
Charahmell <adegoketemiloluwa3@gmail.com>
2026-05-28 23:16:12 +0100

    messagefixed the catch all route to express v5 allowed

--- Node/index.js
+++ Node/index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.DB_URI)
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // THE CATCH-ALL ROUTE (Must be the very last route in your file!)
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
